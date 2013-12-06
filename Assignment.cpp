5f3c7c81281629a46c97c2f81b371d2208ba04fe
SargeCon <conorsargent@hotmail.com>
2013-12-06 17:51:47 +0000

    Set up physics and game world. Added in a bos for testing. Added gravity

---
+++ Assignment.cpp
@@ -0,0 +1,73 @@
+#include "Assignment.h"
+#include "PhysicsController.h"
+#include "Sphere.h"
+#include "PhysicsCamera.h"
+#include "Box.h"
+#include "Cylinder.h"
+#include "Steerable3DController.h"
+#include "Ground.h"
+#include "Content.h"
+#include <btBulletDynamicsCommon.h>
+#include <gtc/quaternion.hpp>
+#include <gtx/quaternion.hpp>
+#include <gtx/euler_angles.hpp>
+#include <gtx/norm.hpp>
+#include "VectorDrawer.h"
+#include "Utils.h"
+
+using namespace BGE;
+
+Assignment::Assignment(void)
+{
+	physicsFactory = NULL;
+	dynamicsWorld = NULL;
+	broadphase = NULL;
+	dispatcher = NULL;
+	solver = NULL;
+	fullscreen = false;
+
+}
+
+Assignment::~Assignment(void)
+{
+}
+
+bool Assignment::Initialise() 
+{
+	collisionConfiguration = new btDefaultCollisionConfiguration();
+    dispatcher = new btCollisionDispatcher(collisionConfiguration);
+ 
+    // The world.
+	btVector3 worldMin(-1000,-1000,-1000);
+	btVector3 worldMax(1000,1000,1000);
+	broadphase = new btAxisSweep3(worldMin,worldMax);
+	solver = new btSequentialImpulseConstraintSolver();
+	dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher,broadphase,solver,collisionConfiguration);
+    dynamicsWorld->setGravity(btVector3(0,-10,0));
+
+	physicsFactory = make_shared<PhysicsFactory>(dynamicsWorld);
+
+	physicsFactory->CreateGroundPhysics();
+	physicsFactory->CreateCameraPhysics();
+
+	shared_ptr<PhysicsController> box1 = physicsFactory->CreateBox(1,1,4, glm::vec3(5, 5, 0), glm::quat()); 
+
+	if (!Game::Initialise()) {
+		return false;
+	}
+
+	camera->GetController()->position = glm::vec3(0,10, 20);
+	
+	return true;
+}
+
+void BGE::Assignment::Update(float timeDelta)
+{
+	dynamicsWorld->stepSimulation(timeDelta,100);
+	Game::Update(timeDelta);
+}
+
+void BGE::Assignment::Cleanup()
+{
+	Game::Cleanup();
+}
---
+++ Assignment.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "Game.h"
+#include "PhysicsController.h"
+#include "PhysicsFactory.h"
+#include <btBulletDynamicsCommon.h>
+
+namespace BGE
+{
+	class Assignment :
+		public Game
+	{
+	private:
+		btBroadphaseInterface* broadphase;
+ 
+		// Set up the collision configuration and dispatcher
+		btDefaultCollisionConfiguration * collisionConfiguration;
+		btCollisionDispatcher * dispatcher;
+ 
+		// The actual physics solver
+		btSequentialImpulseConstraintSolver * solver;
+
+	public:
+		Assignment(void);
+		~Assignment(void);
+		bool Initialise();
+		void Update(float timeDelta);
+		void Cleanup();
+		
+		// The world.
+		std::shared_ptr<PhysicsFactory> physicsFactory;
+		btDiscreteDynamicsWorld * dynamicsWorld;
+	};
+}
