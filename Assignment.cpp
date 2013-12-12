e038d8a6b6521bd01bdceb06cc23fa6fd30b700a
SargeCon <conorsargent@hotmail.com>
2013-12-12 20:20:59 +0000

    Added in a ragdoll, not completed yet

--- Assignment.cpp
+++ Assignment.cpp
@@ -1,12 +1,4 @@
 #include "Assignment.h"
-#include "PhysicsController.h"
-#include "Sphere.h"
-#include "Box.h"
-#include "Cylinder.h"
-#include "Steerable3DController.h"
-#include "Ground.h"
-#include "Content.h"
-#include "PhysicsCamera.h"
 #include <btBulletDynamicsCommon.h>
 #include <gtc/quaternion.hpp>
 #include <gtx/quaternion.hpp>
@@ -16,6 +8,8 @@
 #include "Utils.h"
 
 using namespace BGE;
+
+//std::shared_ptr<PhysicsFactory> physicsFactory;
 
 Assignment::Assignment(void)
 {
@@ -27,6 +21,7 @@ Assignment::Assignment(void)
 	dispatcher = NULL;
 	solver = NULL;
 	fullscreen = false;
+
 
 
 }
@@ -47,14 +42,37 @@ bool Assignment::Initialise()
 	broadphase = new btAxisSweep3(worldMin,worldMax);
 	solver = new btSequentialImpulseConstraintSolver();
 	dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher,broadphase,solver,collisionConfiguration);
-    dynamicsWorld->setGravity(btVector3(0,-10,0));
+    dynamicsWorld->setGravity(btVector3(0,0,0));
 
-	physicsFactory = make_shared<PhysicsFactory>(dynamicsWorld);
-
+	physicsFactory = make_shared<Create>(dynamicsWorld);
+	//physicsFactory2 = make_shared<Create>(dynamicsWorld);
+	//physicsFactory2->CreateCameraPhysics();
 	physicsFactory->CreateGroundPhysics();
-	physicsFactory->CreateCameraPhysics();
 
-	shared_ptr<PhysicsController> box1 = physicsFactory->CreateBox(1,1,4, glm::vec3(5, 5, 0), glm::quat()); 
+	//shared_ptr<PhysicsController> box1 = physicsFactory->CreateBox(1,1,4, glm::vec3(5, 5, 0), glm::quat()); 
+	head = physicsFactory->CreateSphere(2,30,glm::vec3(20, 16, 0), glm::quat());
+	body = physicsFactory->CreateBox(2,6,3,20, glm::vec3(20, 10, 0), glm::quat()); 
+	arm1 = physicsFactory->CreateBox(1,1,3,10, glm::vec3(20, 12, 5), glm::quat()); 
+	//forearm1 = physicsFactory->CreateBox(1,1,4,10, glm::vec3(20, 12, 8), glm::quat()); 
+	//arm2 = physicsFactory->CreateBox(1,1,3,10, glm::vec3(20, 12, -5), glm::quat()); 
+	//forearm2 = physicsFactory->CreateBox(1,1,4,10, glm::vec3(20, 12, -8), glm::quat());
+	/*leg1 = physicsFactory->CreateBox(1,1,3,15, glm::vec3(20, 10, -5), glm::quat()); 
+	leg2 = physicsFactory->CreateBox(1,1,4,15, glm::vec3(20, 10, -10), glm::quat());*/
+
+	
+	btHingeConstraint * neck = new btHingeConstraint(* head->rigidBody, * body->rigidBody, btVector3(0,-1.5f,0),btVector3(0,4.5f,0), btVector3(0,1,0), btVector3(0,1,0), true);
+
+	btPoint2PointConstraint * shoulder = new btPoint2PointConstraint(*body->rigidBody, *arm1->rigidBody, btVector3(0,1.0f,2.0f),btVector3(0,-1.0f,-2.0f));
+	//btPoint2PointConstraint * elbow = new btPoint2PointConstraint(*arm1->rigidBody, *forearm1->rigidBody, btVector3(0,0,2.0f),btVector3(0,0,-2.0f));
+	//btPoint2PointConstraint * shoulder2 = new btPoint2PointConstraint(*body->rigidBody, *arm2->rigidBody, btVector3(0,1.5f,-2.0f),btVector3(0,1.5f,-2.0f));
+	//btPoint2PointConstraint * elbow2 = new btPoint2PointConstraint(*arm2->rigidBody, *forearm2->rigidBody, btVector3(0,0,2.0f),btVector3(0,0,-2.0f));
+	dynamicsWorld->addConstraint(neck);
+	dynamicsWorld->addConstraint(shoulder);
+	//dynamicsWorld->addConstraint(elbow);
+	//dynamicsWorld->addConstraint(shoulder2);
+	//dynamicsWorld->addConstraint(elbow2);
+	
+
 
 	if (!Game::Initialise()) {
 		return false;
@@ -65,10 +83,9 @@ bool Assignment::Initialise()
 	return true;
 }
 
-shared_ptr<PhysicsController> wheel1;
-shared_ptr<PhysicsController> wheel2;
-shared_ptr<PhysicsController> wheel3;
-shared_ptr<PhysicsController> wheel4;
+
+
+btScalar maxMotorImpulse = 1400.f;
 
 void Assignment::Update(float timeDelta)
 {
@@ -76,49 +93,41 @@ void Assignment::Update(float timeDelta)
 	Game * game = Game::Instance();
 
 	float timeToPass = 1.0f / f;
+
 	//spawn a car when space is pressed
-	if (keyState[SDL_SCANCODE_F] && (e > timeToPass))
+	if (keyState[SDL_SCANCODE_T] && (e > timeToPass))
 	{
-		PrintFloat("elapsed ", timeToPass);
-		glm::vec3 position(20,20,0);
-		float width = 15;
+		glm::vec3 position(100,10,0);
+		float width = 12;
 		float height = 2;
 		float length = 5;
+		float chassisMass = 100;
 		float wheelWidth = 1;
-		float wheelRadius = 2;
-		float wheelOffset = 2.0f;
+		float wheelRadius = 1.5f;
+		float wheelMass = 25;
+		float wheelOffset = 1.0f;
 
-		shared_ptr<PhysicsController> chassis = physicsFactory->CreateBox(width, height, length, position, glm::quat());
+		shared_ptr<PhysicsController> chassis = physicsFactory->CreateBox(width, height, length, chassisMass, position, glm::quat());
 
-		
-		glm::quat q =  glm::angleAxis(glm::half_pi<float>(), glm::vec3(1, 0, 0));
+		frontright= physicsFactory->CreateCylinder(wheelRadius, wheelWidth, wheelMass, glm::vec3(106,10,5), glm::quat());
 
-		glm::vec3 offset;
-		btHingeConstraint * hinge;
+		hinge1 = new btHingeConstraint(* chassis->rigidBody, * frontright->rigidBody, btVector3(6.0f,0.0f,5.0f), btVector3(0,0,0), btVector3(0,0,1), btVector3(0,1,0), true);
+		dynamicsWorld->addConstraint(hinge1);
 
-		offset = glm::vec3(- (width / 2 - wheelRadius), 0, - (length / 2 + wheelOffset));
-		wheel1= physicsFactory->CreateCylinder(wheelRadius, wheelWidth, position + offset, q);	 
-		hinge = new btHingeConstraint(* chassis->rigidBody, * wheel1->rigidBody, GLToBtVector(offset),btVector3(0,0, 0), btVector3(0,0,1), btVector3(0,1,0), true);
-		dynamicsWorld->addConstraint(hinge);
+		backright= physicsFactory->CreateCylinder(wheelRadius, wheelWidth,wheelMass, glm::vec3(106,10,5), glm::quat());
+		hinge2 = new btHingeConstraint(* chassis->rigidBody, * backright->rigidBody, btVector3(6.0f,0.0f,-5.0f),btVector3(0,0, 0), btVector3(0,0,1), btVector3(0,1,0), true);
+		dynamicsWorld->addConstraint(hinge2);
 
-		offset = glm::vec3(+ (width / 2 - wheelRadius), 0, - (length / 2 + wheelOffset));
-		wheel2= physicsFactory->CreateCylinder(wheelRadius, wheelWidth, glm::vec3(position.x + (width / 2) - wheelRadius, position.y, position.z - (length / 2) - wheelWidth), q);
-		hinge = new btHingeConstraint(* chassis->rigidBody, * wheel2->rigidBody, GLToBtVector(offset),btVector3(0,0, 0), btVector3(0,0,1), btVector3(0,1,0), true);
-		dynamicsWorld->addConstraint(hinge);
+		backleft= physicsFactory->CreateCylinder(wheelRadius, wheelWidth, wheelMass, glm::vec3(106,10,5), glm::quat()); 
+		hinge3 = new btHingeConstraint(* chassis->rigidBody, * backleft->rigidBody, btVector3(-6.0f,0.0f,-5.0f),btVector3(0,0, 0), btVector3(0,0,1), btVector3(0,1,0), true);
+		dynamicsWorld->addConstraint(hinge3);
 
-		offset = glm::vec3(- (width / 2 - wheelRadius), 0, + (length / 2 + wheelOffset));
-		wheel3= physicsFactory->CreateCylinder(wheelRadius, wheelWidth, position + offset, q);	 
-		hinge = new btHingeConstraint(* chassis->rigidBody, * wheel3->rigidBody, GLToBtVector(offset),btVector3(0,0, 0), btVector3(0,0,1), btVector3(0,1,0), true);
-		dynamicsWorld->addConstraint(hinge);
-
-		offset = glm::vec3(+ (width / 2 - wheelRadius), 0, + (length / 2 + wheelOffset));
-		wheel4= physicsFactory->CreateCylinder(wheelRadius, wheelWidth, position + offset, q);	 
-		hinge = new btHingeConstraint(* chassis->rigidBody, * wheel4->rigidBody, GLToBtVector(offset),btVector3(0,0, 0), btVector3(0,0,1), btVector3(0,1,0), true);
-		dynamicsWorld->addConstraint(hinge);
-
-		
-
+		frontleft= physicsFactory->CreateCylinder(wheelRadius, wheelWidth, wheelMass, glm::vec3(106,10,5), glm::quat()); 
+		hinge4 = new btHingeConstraint(* chassis->rigidBody, * frontleft->rigidBody, btVector3(-6.0f,0.0f,5.0f),btVector3(0,0, 0), btVector3(0,0,1), btVector3(0,1,0), true);
+		dynamicsWorld->addConstraint(hinge4);
+		PrintText("Test");
 		e = 0.0f;
+		
 	}
 	else
 	{
@@ -127,17 +136,36 @@ void Assignment::Update(float timeDelta)
 
 	if (keyState[SDL_SCANCODE_UP])
 	{
+		
+		//wheel1->rigidBody->applyTorque(GLToBtVector(glm::vec3(0.0f,0.0f,200.0f)));
+		//wheel2->rigidBody->applyTorque(GLToBtVector(glm::vec3(0.0f,0.0f,1000.0f)));
+		backright->rigidBody->applyTorque(GLToBtVector(glm::vec3(0.0f,0.0f,1000.0f)));
+		backleft->rigidBody->applyTorque(GLToBtVector(glm::vec3(0.0f,0.0f,1000.0f)));
+
 		PrintText("Up");
-		wheel1->rigidBody->applyTorque(GLToBtVector(glm::vec3(0.0f,0.0f,100.0f)));
-		wheel2->rigidBody->applyTorque(GLToBtVector(glm::vec3(0.0f,0.0f,100.0f)));
-		wheel3->rigidBody->applyTorque(GLToBtVector(glm::vec3(0.0f,0.0f,100.0f)));
-		wheel4->rigidBody->applyTorque(GLToBtVector(glm::vec3(0.0f,0.0f,100.0f)));
 		
 	}
 	if(keyState[SDL_SCANCODE_DOWN])
 	{
 		PrintText("Down");
-		wheel1->rigidBody->applyTorque(GLToBtVector(glm::vec3(0.0f,0.0f,-10.0f)));
+		frontright->rigidBody->applyTorque(GLToBtVector(glm::vec3(0.0f,0.0f,-50.0f)));
+		backright->rigidBody->applyTorque(GLToBtVector(glm::vec3(0.0f,0.0f,-50.0f)));
+		backleft->rigidBody->applyTorque(GLToBtVector(glm::vec3(0.0f,0.0f,-50.0f)));
+		frontleft->rigidBody->applyTorque(GLToBtVector(glm::vec3(0.0f,0.0f,-50.0f)));
+		
+	}
+	if(keyState[SDL_SCANCODE_LEFT])
+	{
+		PrintText("Left");
+		frontright->rigidBody->applyTorque(GLToBtVector(glm::vec3(2000.0f,0.0f,0.0f)));
+		frontleft->rigidBody->applyTorque(GLToBtVector(glm::vec3(2000.0f,0.0f,0.0f)));
+		
+	}
+	if(keyState[SDL_SCANCODE_RIGHT])
+	{
+		PrintText("Right");
+		frontright->rigidBody->applyTorque(GLToBtVector(glm::vec3(-2000.0f,0.0f,0.0f)));
+		frontleft->rigidBody->applyTorque(GLToBtVector(glm::vec3(-4000.0f,0.0f,0.0f)));
 		
 	}
 
--- Assignment.h
+++ Assignment.h
@@ -1,8 +1,8 @@
 #pragma once
 #include "Game.h"
 #include "PhysicsController.h"
-
-#include "PhysicsFactory.h"
+#include "Create.h"
+//#include "PhysicsFactory.h"
 #include <btBulletDynamicsCommon.h>
 
 namespace BGE
@@ -26,11 +26,31 @@ namespace BGE
 		bool Initialise();
 		void Update(float timeDelta);
 		void Cleanup();
+		void createCar();
 		
 		// The world.
-		std::shared_ptr<PhysicsFactory> physicsFactory;
+		std::shared_ptr<Create> physicsFactory;
+		//std::shared_ptr<PhysicsFactory> physicsFactory2;
 		btDiscreteDynamicsWorld * dynamicsWorld;
 		float e;
 		float f;
+		std::shared_ptr<PhysicsController> frontright;
+		std::shared_ptr<PhysicsController> backright;
+		std::shared_ptr<PhysicsController> backleft;
+		std::shared_ptr<PhysicsController> frontleft;
+		btHingeConstraint * hinge1;
+		btHingeConstraint * hinge2;
+		btHingeConstraint * hinge3;
+		std::shared_ptr<PhysicsController> body;
+		std::shared_ptr<PhysicsController> arm1;
+		std::shared_ptr<PhysicsController> forearm1;
+		std::shared_ptr<PhysicsController> arm2;
+		std::shared_ptr<PhysicsController> forearm2;
+		std::shared_ptr<PhysicsController> head;
+		std::shared_ptr<PhysicsController> leg1;
+		std::shared_ptr<PhysicsController> leg2;
+
+		btHingeConstraint * hinge4;
+		
 	};
 }
